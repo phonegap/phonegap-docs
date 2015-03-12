1e21075ef80a80dc1742a97f7150b499acc5b37d
Michael Brooks <michael@michaelbrooks.ca>
2015-03-12 14:51:22 -0700

    [#44] Passively convert name.md to name.html.md.

--- docpad.js
+++ docpad.js
@@ -47,14 +47,29 @@ module.exports = {
   "collections": {
     /**
      * Set default layout for all markdown documents.
-     * Added side-effect that markdown documents do not require
-     * the .html.md convention, since the layout defines HTML output.
      */
     docs: function() {
       return this.getCollection('documents')
         .findAllLive({ extension: 'md' }, [{ filename: -1 }])
         .on('add', function(model) {
           model.setMetaDefaults({ 'layout': 'default' });
+        });
+    },
+
+    /**
+     * Update all `name.md` to `name.html.md`.
+     * Question: Is there a better way to accomplish this?
+     */
+    md: function() {
+      return this.getCollection('documents')
+        .findAllLive({ extensions: ['md'] }, [{ filename: -1 }])
+        .on('add', function(model) {
+          model.set('filename', model.get('filename').replace('.md', '.html.md'));
+          model.set('extensions', ['html', 'md']);
+          model.set('outExtension', 'html');
+          model.set('outPath', model.get('outPath').replace('.md', '.html.md'));
+          model.set('outFilename', model.get('outFilename').replace('.md', '.html.md'));
+          model.set('outContentType', 'text/html');
         });
     }
   }
