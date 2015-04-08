ccad82135545bfd98e05ce3e105a548a7648d08c
Michael Brooks <michael@michaelbrooks.ca>
2015-04-08 15:37:57 -0700

    [spec] Add Jasmine test runner.

---
+++ spec/index.spec.js
@@ -0,0 +1,15 @@
+/*!
+ * Module dependencies.
+ */
+
+/**
+ * Specification.
+ *
+ * There are no tests at the moment.
+ */
+
+describe('test', function() {
+    it('should pass', function() {
+        expect(true).toEqual(true);
+    });
+});
