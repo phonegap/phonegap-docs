d60a6b5ed44cd062354e9881ed8356a739eb89cc
garthdb <garthdb@gmail.com>
2015-05-28 03:39:14 -0600

    iniitial mobile nav

---
+++ assets/scripts/main.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function(event) {
+  var toggleButton = Sizzle('.mobile-docs-toggle')[0];
+  var sidebar = Sizzle ('aside.sidebar')[0];
+  toggleButton.addEventListener("click", function(e){
+    sidebar.classList.toggle('open');
+  });
+});
