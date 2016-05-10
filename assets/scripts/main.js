e9de466d360ff71e25b0541f9001eecc5f871f2f
Holly Schinsky <hollyschinsky@gmail.com>
2016-05-10 08:03:28 -0400

    Updates to add scrolling and styling updates to mobile menu. Removed menu subcategories for tutorials, other minor cleanup.

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,7 +1,9 @@
 document.addEventListener('DOMContentLoaded', function(event) {
   var toggleButton = Sizzle('.mobile-docs-toggle')[0];
   var sidebar = Sizzle ('aside.sidebar')[0];
-  toggleButton.addEventListener("click", function(e){
-    sidebar.classList.toggle('open');
-  });
+  
+  toggleButton.addEventListener("click", function(e){           
+    sidebar.classList.toggle('open');        
+  });    
+  
 });
